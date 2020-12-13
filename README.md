eed087140f1147ff5845daed0a74e9aab5fe07c5
Densuke-fitness <densukefitness2020@gmail.com>
2020-12-13 16:17:07 +0900

    readmeの更新

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 pythonフォルダ内に各問題のフォルダがあります。
 (僕が解説した問題はqittaに投函します)
 
-# 問題元のqitta
-https://qiita.com/saba/items/affc94740aff117d2ca9
+## 問題元のqitta
+- [蟻本をPythonで (初級編)](https://qiita.com/saba/items/affc94740aff117d2ca9)
+
+## Densuke-fitnessが解説したアルゴリム記事
+- [アルゴリズム力UP!『ABC 045 C たくさんの数式 』を解いてみよう](https://qiita.com/Densuke-fitness/items/419665fdb70806875bc4)
--- python/1-1-ABC 045 C たくさんの数式/test1-1.py
+++ python/1-1-ABC 045 C たくさんの数式/test1-1.py
@@ -1,37 +1,38 @@
-def sum_all(num: int) -> int :
 
+
+def solve_many_formulas(num: int) -> int :
+    # ==============================================================
+    # return: ある整数値を代入すると、数と数の間に+を入れてできる数式の和の合計を返す
+    # pre: 125
+    # post:　176 = (125) + (26 = 1+25) + (17 = 12+5) + (8 = 1+2+5) 
+    # ==============================================================
+
+    # ★ step1 整数値を文字列にし、数と数の間に+を入れられるようにする
+    # ★ step2 文字列にした数の長さを取り、インデックスで+をいれる場所を指定できるようにする。
+    # ★ step3 再帰関数を用いて、+が入れられる場所を、+をいれる/入れない時で全通り調べる
+    # ★ step4 数と数の間に+を入れてできる数式の和を返す
+
+    #整数値を文字列にする
     str_num = str(num)
+    #文字列にした数字の長さを取る
     len_str_num = len(str_num)
 
-    #iがlen_str_numの文字列のインデックス, sentenceが+と数値を含む文字列 
-    def dfs(i: int , sentence: str) -> int: 
-        # 再起が最後まで終了した時
-        if i == len_str_num - 1:
-            #＋で挟んでいる文字の合計値を出す
+    #idxがlen_str_numの文字列のインデックス, sentenceが+と数値を含む文字列
+    def dfs(idx: int , sentence: str, str_num: str, len_str_num: int) -> int: 
+
+        # 文字列idxが一番後ろに到達したかどうかを判定
+        if idx == len_str_num - 1:
+            #+で挟んでいるところで数字をわけ、数値に変更し合計値を出す
             return sum(list(map(int, sentence.split("+"))))
         #間に+を入れるか入れないかで分岐させ、全通り検索できるようにする
-        return dfs(i + 1, sentence + "+" + str_num[i + 1]) + dfs(i + 1, sentence + str_num[i + 1])
+        #最終的に佐伯関数からreturnした値を足算していく
+        return (
+                dfs(idx + 1, sentence + "+" + str_num[idx + 1], str_num, len_str_num) 
+              + dfs(idx + 1, sentence + str_num[idx + 1], str_num, len_str_num) 
+        )
      
-    #初期値として文字列のインデックス, 文字列にした数値の0行目の値を代入
-    return dfs(0, str_num[0])
+    #初期値として文字列の最初のインデックス, 文字列にした数値の0行目の値, 
+    return dfs(0, str_num[0], str_num, len_str_num)
 
-
-print(sum_all(125))
-
-
-import unittest
-class TestSumAll(unittest.TestCase):
-
-    def test_sum_all(self):
-        # Case : normal
-        val = 1234
-        self.assertEqual(sum_all(val), 1736)
-
-        # Case : 0
-        val = 0
-        self.assertEqual(sum_all(val), 0)
-
-if __name__ == "__main__":
-    unittest.main()
-
-#atcoderテストケースもあるんだね
+N = int(input())
+print(solve_many_formulas(N))
